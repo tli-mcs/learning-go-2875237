c815f07f1a1f003b3314f88b9bb4cf73ffc0cc85
Ting Li <tli@macquariecloudservices.com>
2024-05-31 20:58:12 +1000

    var

--- practice/main.go
+++ practice/main.go
@@ -4,6 +4,21 @@ import (
 	"fmt"
 )
 
+const aConst int = 64
+
 func main() {
-	fmt.Println("Hello from Go!")
+	var aString string = "The format is so strange!"
+	fmt.Println(aString)
+	fmt.Printf("The variable is of type: %T\n", aString)
+
+	var anInteger int = 42
+	fmt.Println(anInteger)
+
+	var anotherString = "This is anothe string"
+	fmt.Println(anotherString)
+	fmt.Printf("The variable is of type: %T\n", anotherString)
+
+	myString := "This is a string too"
+	fmt.Println(myString)
+	fmt.Printf("The variable is of type: %T\n", myString)
 }
