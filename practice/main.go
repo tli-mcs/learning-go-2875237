6a0ba8e7ac4aa10caec80cab5f56799b74022e9c
Ting Li <tli@macquariecloudservices.com>
2024-06-01 12:19:04 +1000

    pointer

--- practice/main.go
+++ practice/main.go
@@ -2,14 +2,18 @@ package main
 
 import (
 	"fmt"
-	"time"
 )
 
 func main() {
-	n := time.Now()
-	fmt.Println("I recorded the time as", n)
+	anInt := 42
+	var p = &anInt
+	fmt.Println(*p)
 
-	t := time.Date(2019, time.November, 10, 23, 0, 0, 0, time.UTC)
-	fmt.Println("Go lunched at ", t)
-	fmt.Println(t.Format(time.Kitchen))
+	value1 := 42.13
+	pointer1 := &value1
+
+	fmt.Println(*pointer1)
+	*pointer1 = *pointer1 / 31
+	fmt.Println(*pointer1)
+	fmt.Println(value1)
 }
